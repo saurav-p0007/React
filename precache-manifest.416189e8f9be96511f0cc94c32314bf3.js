8f89fc8053d0909a1f341713f6fe5fa306759f42
saurav-p0007 <saurav.p0007@gmail.com>
2020-08-27 00:09:39 +0530

    Updates

---
+++ precache-manifest.416189e8f9be96511f0cc94c32314bf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b8487b4c6bab5fee8091170a18845e5",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "570b8bb6b673d2912c0b",
+    "url": "/React/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8c87eb4ef4c72b65d696",
+    "url": "/React/static/js/2.a0292298.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React/static/js/2.a0292298.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "570b8bb6b673d2912c0b",
+    "url": "/React/static/js/main.f7b7a995.chunk.js"
+  },
+  {
+    "revision": "4360c1dc01ab25ff3c18",
+    "url": "/React/static/js/runtime-main.eb4553aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React/static/media/logo.5d5d9eef.svg"
+  }
+]);
